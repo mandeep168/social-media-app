d87b1d1708f8c864133033d87c9ead68b74d7399
mandeep168 <ichbinmandeep@gmail.com>
2024-12-24 11:16:39 +0530

    changes

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,5 +4,5 @@ exports.connectDatabase = () => {
     mongoose
     .connect(process.env.MONGO_URI)
     .then((con) => console.log(`Database connected: ${con.connection.host}`))
-    .catch((err) => console.log(err));
+    .catch((err) => console.log(`Erorr in Database Connection: ${err}`));
 };
